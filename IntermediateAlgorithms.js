a3cc21682bb1828c9cbcf0abde89c759ec9c452e
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-14 15:31:09 -0500

    Start document and write first algorithm

---
+++ IntermediateAlgorithms.js
@@ -0,0 +1,25 @@
+/*****************************************************************
+ All of the algorithms for freeCodeCamp's intermediate algorithms
+ testable using jasmine.
+******************************************************************/
+
+
+/*
+Sum of Numbers in Range
+-----------------------
+
+We'll pass you an array of two numbers return the sum of those two numbers plus
+the sum of all the numbers between them. The lowest number will not always come first.
+*/
+
+function sumAll(arr) {
+    var lowerNumber = (arr[0] < arr[1]) ? arr[0] : arr[1];
+    var higherNumber = (arr[0] > arr[1]) ? arr[0] : arr[1];
+    var sum = 0;
+
+    for (let i = lowerNumber; i < higherNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
