cd6b46778e5415118755c53dc89b3edf3840a1ca
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-14 16:52:04 -0500

    Setup tests and function, still need to fill arrays

--- tests.js
+++ tests.js
@@ -4,18 +4,54 @@
 
 describe("Sum of Numbers in Range", function() {
     it("should return a number", function() {
-        expect(sumAll([1,4])).toEqual(jasmine.any(Number));
+        expect(sumAll([1, 4])).toEqual(jasmine.any(Number));
     });
     it("sumAll([1, 4]) should return 10", function() {
-        expect(sumAll([1,4])).toBe(10);
+        expect(sumAll([1, 4])).toBe(10);
     });
     it("sumAll([4, 1]) should return 10", function() {
-        expect(sumAll([4,1])).toBe(10);
+        expect(sumAll([4, 1])).toBe(10);
     });
     it("sumAll([5, 10]) should return 45", function() {
-        expect(sumAll([5,10])).toBe(45);
+        expect(sumAll([5, 10])).toBe(45);
     });
     it("sumAll([10,5]) should return 45", function() {
-        expect(sumAll([10,5])).toBe(45);
+        expect(sumAll([10, 5])).toBe(45);
+    });
+})
+
+describe("Diff Two Arrays", function() {
+    it("should return an array", function() {
+        expect(diffArray([], [])).toEqual(jasmine.any(Array));
+    });
+    it("diffArray([],[]) should return an array with one item", function() {
+        expect(diffArray([], []).length).toBe(1);
+    });
+    it("diffArray([],[]) should return []", function() {
+        expect(diffArray([], [])).toBe([]);
+    });
+    it("diffArray([],[]) should return an array with two items", function() {
+        expect(diffArray([], []).length).toBe(2);
+    });
+    it("diffArray([],[]) should return an empty array", function() {
+        expect(diffArray([], [])).toBe([]);
+    });
+    it("diffArray([],[]) should return an array with one item", function() {
+        expect(diffArray([], []).length).toBe(1);
+    });
+    it("diffArray([],[]) should return [4]", function() {
+        expect(diffArray([], [])).toBe([]);
+    });
+    it("diffArray([],[]) should return an array with two items", function() {
+        expect(diffArray([], []).length).toBe(2);
+    });
+    it("diffArray([],[]) should return ['piglet', 4]", function() {
+        expect(diffArray([], [])).toBe(['piglet', 4]);
+    });
+    it("diffArray([], ['snuffleupagus', 'cookie monster', 'elmo']) should return an array with three items", function() {
+        expect(diffArray([], []).length).toBe(3);
+    });
+    it("diffArray([], ['snuffleupagus', 'cookie monster', 'elmo']) should return ['snuffleupagus', 'cookie monster', 'elmo']", function() {
+        expect(diffArray([], [])).toBe(['snuffleupagus', 'cookie monster', 'elmo']);
     });
 })
