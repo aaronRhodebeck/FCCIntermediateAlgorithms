340da183345b1f727131c230fff1397db9f19863
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-16 18:48:47 -0500

    Setup function

--- IntermediateAlgorithms.js
+++ IntermediateAlgorithms.js
@@ -50,3 +50,17 @@ function diffArray(arr1, arr2) {
         return differentElements;
     }
 }
+
+/*
+Check For Palindromes
+---------------------
+
+Return true if the given string is a palindrome. Otherwise, return false.
+*Note*
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols)
+and turn everything into the same case (lower or upper case) in order to check for palindromes.
+*/
+
+function palindrome(str) {
+
+}
