514b2cad2e8bfd1bf697cf035d149b37ae3d2b9c
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-18 14:46:10 -0500

    Setup function and tests for Roman Numeral Converter

--- IntermediateAlgorithms.js
+++ IntermediateAlgorithms.js
@@ -145,3 +145,16 @@ function spinalCase(str) {
         return " " + match.toLowerCase();
     }
 }
+
+/*
+Roman Numeral Converter
+-----------------------
+
+ Convert the given number into a roman numeral.
+
+ All roman numerals answers should be provided in upper-case.
+ */
+
+function convertToRoman(num) {
+
+}
