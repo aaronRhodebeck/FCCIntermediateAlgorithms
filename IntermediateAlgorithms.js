ef5049be2c0adc10d59f6b2e88f9ec4e1365668a
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-18 11:19:10 -0500

    Setup function and tests for whatIsInAName()

--- IntermediateAlgorithms.js
+++ IntermediateAlgorithms.js
@@ -8,9 +8,9 @@
 Sum of Numbers in Range
 -----------------------
 
-We'll pass you an array of two numbers return the sum of those two numbers plus
-the sum of all the numbers between them. The lowest number will not always come first.
-*/
+ We'll pass you an array of two numbers return the sum of those two numbers plus
+ the sum of all the numbers between them. The lowest number will not always come first.
+ */
 
 function sumAll(arr) {
     var lowerNumber = (arr[0] < arr[1]) ? arr[0] : arr[1];
@@ -28,10 +28,10 @@ function sumAll(arr) {
 Diff Two Arrays
 ---------------
 
-Compare two arrays and return a new array with any items only found in one of the two
-given arrays but not both. In other words, return the symmetric difference of the two 
-arrays.
-*/
+ Compare two arrays and return a new array with any items only found in one of the two
+ given arrays but not both. In other words, return the symmetric difference of the two 
+ arrays.
+ */
 
 function diffArray(arr1, arr2) {
     var diffElem = [];
@@ -55,11 +55,11 @@ function diffArray(arr1, arr2) {
 Check For Palindromes
 ---------------------
 
-Return true if the given string is a palindrome. Otherwise, return false.
-*Note*
-You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols)
-and turn everything into the same case (lower or upper case) in order to check for palindromes.
-*/
+ Return true if the given string is a palindrome. Otherwise, return false.
+ *Note*
+ You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols)
+ and turn everything into the same case (lower or upper case) in order to check for palindromes.
+ */
 
 function palindrome(str) {
     var letterArray = str.toLowerCase().replace(/[^a-z0-9]/g, "").split("");
@@ -70,13 +70,13 @@ function palindrome(str) {
 Seek and Destroy
 ----------------
 
-You will be provided with an initial array (the first argument in the destroyer function), 
-followed by one or more arguments. Remove all elements from the initial array that are of 
-the same value as these arguments.
+ You will be provided with an initial array (the first argument in the destroyer function), 
+ followed by one or more arguments. Remove all elements from the initial array that are of 
+ the same value as these arguments.
 
-*Note*
-You have to use the arguments object.
-*/
+ *Note*
+ You have to use the arguments object.
+ */
 
 function destroyer(arr) {
     var args = [...arguments];
@@ -89,4 +89,22 @@ function destroyer(arr) {
     }
 
     return array;
+}
+
+/*
+Wherefore art thou
+------------------
+
+ Make a function that looks through an array of objects (first argument) and returns an array
+ of all objects that have matching name and value pairs (second argument). Each name and value
+ pair of the source object has to be present in the object from the collection if it is to be
+ included in the returned array.
+
+ For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio",
+ last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" },
+ then you must return the third object from the array (the first argument), because it contains 
+ the name and its value, that was passed on as the second argument.
+ */
+function whatIsinAName(collection, source) {
+
 }
