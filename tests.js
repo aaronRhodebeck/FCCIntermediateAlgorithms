51c69c8590f56b35262e32ce31aaeb172a226360
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-18 10:30:27 -0500

    Setup Seek and Destroy tests and function

--- tests.js
+++ tests.js
@@ -94,3 +94,24 @@ describe("Check for Palindromes", function() {
         expect(palindrome("five|\_/|four")).toEqual(false);
     });
 })
+
+describe("Seek and Destroy", function() {
+    it('destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1]', function() {
+        expect(destroyer([1, 2, 3, 1, 2, 3], 2, 3)).toEqual([1, 1]);
+    });
+    it('destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1]', function() {
+        expect(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)).toEqual([1, 5, 1]);
+    });
+    it('destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1]', function() {
+        expect(destroyer([3, 5, 1, 2, 2], 2, 3, 5)).toEqual([1]);
+    });
+    it('destroyer([2, 3, 2, 3], 2, 3) should return an empty array', function() {
+        expect(destroyer([2, 3, 2, 3], 2, 3)).toEqual([]);
+    });
+    it('destroyer(["tree", "hamburger", 53], "tree", 53) should return ["hamburger"]', function() {
+        expect(destroyer(["tree", "hamburger", 53], "tree", 53)).toEqual(["hamburger"]);
+    });
+    it('destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan") should return [12,92,65]', function() {
+        expect(destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan")).toEqual([12, 92, 65]);
+    })
+})
