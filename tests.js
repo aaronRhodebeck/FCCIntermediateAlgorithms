cd82fe3be351b35e87b6937a0adce600cb47630b
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-19 14:04:11 -0500

    Setup tests and function for Sorted Union

--- tests.js
+++ tests.js
@@ -373,3 +373,20 @@ describe("Missing Letters", function() {
         expect(fearNotLetter("abcdefghijklmnopqrstuvwxyz")).toEqual(undefined);
     });
 })
+
+describe("Sorted Union", function() {
+    it('uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4]', function() {
+        expect(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])).toEqual([1, 3, 2, 5, 4]);
+    });
+    it('uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]]', function() {
+        expect(uniteUnique([1, 3, 2], [1, [5]], [2, [4]])).toEqual([1, 3, 2, [5],
+            [4]
+        ]);
+    });
+    it('uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5]', function() {
+        expect(uniteUnique([1, 2, 3], [5, 2, 1])).toEqual([1, 2, 3, 5]);
+    });
+    it('uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8]', function() {
+        expect(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])).toEqual([1, 2, 3, 5, 4, 6, 7, 8]);
+    });
+})
