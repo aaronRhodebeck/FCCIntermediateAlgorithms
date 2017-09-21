c7a976d137e548ae9b882f4148da6d6d992523ce
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-21 10:35:41 -0500

    Setup tests and function for Drop It

--- tests.js
+++ tests.js
@@ -469,3 +469,24 @@ describe("Smallest Common Multiple", function() {
         expect(smallestCommons([23, 18])).toEqual(6056820);
     });
 })
+
+describe("Drop It", function() {
+    it('dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4]', function() {
+        expect(dropElements([1, 2, 3, 4], function(n) { return n >= 3; })).toEqual([3, 4]);
+    });
+    it('dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1]', function() {
+        expect(dropElements([0, 1, 0, 1], function(n) { return n === 1; })).toEqual([1, 0, 1]);
+    });
+    it('dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3]', function() {
+        expect(dropElements([1, 2, 3], function(n) { return n > 0; })).toEqual([1, 2, 3]);
+    });
+    it('dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return an empty array', function() {
+        expect(dropElements([1, 2, 3, 4], function(n) { return n > 5; })).toEqual([]);
+    });
+    it('dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4]', function() {
+        expect(dropElements([1, 2, 3, 7, 4], function(n) { return n > 3; })).toEqual([7, 4]);
+    });
+    it('dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2]', function() {
+        expect(dropElements([1, 2, 3, 9, 2], function(n) { return n > 2; })).toEqual([3, 9, 2]);
+    });
+})
