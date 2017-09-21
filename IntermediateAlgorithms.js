bf1466a8064a9636557f81b7fd2025e85cca7887
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-21 11:26:34 -0500

    Setup tests and function for Binary Agents

--- IntermediateAlgorithms.js
+++ IntermediateAlgorithms.js
@@ -640,3 +640,14 @@ function steamrollArray(arr) {
         return accumulator;
     }
 }
+
+/*
+Binary Agents
+-------------
+
+ Return an English translated sentence of the passed binary string.
+
+ The binary string will be space separated. */
+function binaryAgent(str) {
+
+}
